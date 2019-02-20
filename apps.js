035b1bcfafc83f0e44301f3aadc1eb9edce7ab38
rrobinson18 <robinson.rakesha@gmail.com>
2019-02-20 15:17:38 -0500

    first commit

---
+++ apps.js
@@ -0,0 +1,46 @@
+// name = 'Rakesha McIntyre';
+// var name;
+// console.log(name);
+
+// setName();
+// function setName () {
+//     var name = 'Covalence';
+//     console.log(name);
+// }
+
+// console.log('I like tacos!');
+// let avg = findAvg(2, 2);
+// console.log('I like juice', avg);
+// function findAvg(a, b) {
+//     console.log('I like tacos and juice');
+//     var answer = (a + b) / 2;
+    // return answer;
+//}
+
+let fruits = ['strawberry', 'pinapples', 'grapes'];
+let leastFav = 'blackberries';
+
+function printFruits() {
+    let favFruit = fruits[1];
+    console.log(fruits[0]);
+
+function printFavFruit() {
+    console.log(favFruit);
+    }
+    printFavFruit();
+  
+}
+console.log(leastFav);
+
+printFruits();
+
+someFunc();
+function someFunc() {
+console.log('Hello, Kesha');
+}
+
+let otherFunc = function() {
+    alert('Boom Shok a loka');
+}
+
+otherFunc();
